de61c79a0c84c1d80133b2f7d520923a796d10f2
steel johnson <steelvargas@gmail.com>
2026-07-17 12:02:25 -0400

    update ver

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.steel101.musicplayer"
         minSdk = 34
         targetSdk = 37
-        versionCode = 6
-        versionName = "1.0.6"
+        versionCode = 7
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
