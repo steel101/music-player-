c44945ce4be8d6596a02697834fd53b9c344d2a6
steel johnson <steelvargas@gmail.com>
2026-07-23 03:36:13 -0400

    Bump version code and version name to 1.0.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.steel101.musicplayer"
         minSdk = 31
         targetSdk = 37
-        versionCode = 12
-        versionName = "1.0.12"
+        versionCode = 13
+        versionName = "1.0.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
